2f37f56c2e52318b9fe39508c64985a77ac7fc67
laenzo <amdiatoutimera@hotmail.fr>
2018-11-09 06:27:17 +0100

    Création du projet

---
+++ gestion-biblio-consumer/src/main/java/com/opc/projet/consumer/App.java
@@ -0,0 +1,13 @@
+package com.opc.projet.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ gestion-biblio-model/src/main/java/com/opc/projet/model/App.java
@@ -0,0 +1,13 @@
+package com.opc.projet.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
