8d4e132244a2f9ae02d4b394938de570c18572ae
laenzo <amdiatoutimera@hotmail.fr>
2018-11-26 11:11:56 +0100

    Book entity / BookCopy entity

--- gestion-biblio-webapp/src/main/java/com/opc/projet/gestionbiblio/webapp/entity/Book.java
+++ gestion-biblio-webapp/src/main/java/com/opc/projet/gestionbiblio/webapp/entity/Book.java
@@ -1,18 +1,86 @@
 package com.opc.projet.gestionbiblio.webapp.entity;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Entity
+@Table(name = "book")
 public class Book {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "date_of_publication")
     private Date dateOfPublication;
-    private List<Author> authors;
+
+    // when a book is deleted, all it's copies will be deleted
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
     private List<BookCopy> copies;
 
+    public Book() {
+    }
 
+    public Book(String title, Date dateOfPublication) {
+        this.title = title;
+        this.dateOfPublication = dateOfPublication;
+    }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDateOfPublication() {
+        return dateOfPublication;
+    }
+
+    public void setDateOfPublication(Date dateOfPublication) {
+        this.dateOfPublication = dateOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", dateOfPublication=" + dateOfPublication +
+                '}';
+    }
+
+    public List<BookCopy> getCopies() {
+        return copies;
+    }
+
+    public void setCopies(List<BookCopy> copies) {
+        this.copies = copies;
+    }
+
+    // convenience methods for bi-directional relationship
+
+    public void add (BookCopy bookCopy){
+        if(copies == null){
+            copies = new ArrayList<>();
+        }
+        copies.add(bookCopy);
+        bookCopy.setBook(this);
+    }
 
 }
--- gestion-biblio-webapp/src/main/java/com/opc/projet/gestionbiblio/webapp/entity/BookCopy.java
+++ gestion-biblio-webapp/src/main/java/com/opc/projet/gestionbiblio/webapp/entity/BookCopy.java
@@ -1,12 +1,65 @@
 package com.opc.projet.gestionbiblio.webapp.entity;
 
+import javax.persistence.*;
 import java.util.Date;
 
-
+@Entity
+@Table
 public class BookCopy {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "date_of_purchase")
     private Date dateOfPurchase;
+
+    // a Book is not deleted when the copy is deleted
+    @ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "book_id")
     private Book book;
 
+
+
+    public BookCopy() {
+    }
+
+    public BookCopy(Date dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    public void setDateOfPurchase(Date dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public String toString() {
+        return "BookCopy{" +
+                "id=" + id +
+                ", dateOfPurchase=" + dateOfPurchase +
+                '}';
+    }
 }
